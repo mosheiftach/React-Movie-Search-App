e3f05959300369b9d10ec025c3912ac4db2f74fa
mosheiftach <103565470+mosheiftach@users.noreply.github.com>
2023-01-15 14:08:25 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react"
+import {SearchMoviePage} from "./components/SearchMoviePage";
+import './App.css';
+
+function App() {
+  return (<SearchMoviePage/>);
+}
+
+export default App;
